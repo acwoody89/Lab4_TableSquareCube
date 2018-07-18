956e4119a2aa1a70f304764b16539de01b7a5d76
Alex <acwoody89@gmail.com>
2018-07-18 17:06:00 -0400

    second commit, more comments

--- src/TableSquareCube.java
+++ src/TableSquareCube.java
@@ -10,23 +10,28 @@ public class TableSquareCube {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		// setting an initial condition that will be true so the while loop will
-		// execute at least one time.
+		
 		String cont = "y";
 		int userInput = 0;
 
+		/*
+		 * has the user input a number, any higher than specified breaks the program
+		 */
 		while (cont.equalsIgnoreCase("y")) {
 			System.out.println("*****************************************");
 			System.out.println("Please enter a number from 1 to 99999999");
-			//System.out.println("*****************************************");
 			userInput = scan.nextInt();
 			System.out.println("*****************************************");
 			
+			/*
+			 * here is the column spacing, justifying to the left, so the output is to
+			 * the right of the variables
+			 */
 			if (userInput >= 1) {
 				System.out.printf("%-15.30s  %-15.30s  %-15.30s%n", "Number", "Square", "Cube");
 				System.out.printf("%-15.30s  %-15.30s  %-15.30s%n", "******", "******", "******");
 				
-				mathStuff(userInput);
+				mathStuff(userInput); // calls the mathStuff method I created
 				
 				System.out.println("*****************************************");
 
@@ -48,12 +53,15 @@ public class TableSquareCube {
 		int square = 0;
 		int cube = 0;
 		int input = a;
-
+		
+		/*
+		 * this loop starts at 1, and then would loop a total of whatever was inputed by
+		 * the user 
+		 */
 		for (int i = 1; i <= input; i++) {
-			square = (int) Math.pow(i, 2);
-			cube = (int) Math.pow(i, 3);
+			square = (int) Math.pow(i, 2);  // raises the input to the power of 2
+			cube = (int) Math.pow(i, 3);	// raises the input to the power of 3
 			System.out.printf("%-15s  %-15s  %s%n", i, square, cube);
-			// System.out.println(cube);
 		}
 	}
 
